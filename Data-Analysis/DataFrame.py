b06e1cb9fbb38323c1ed62b9c064589b77a84e58
tusikalanse <615822545@qq.com>
2020-03-13 11:13:22 +0800

    add Chapter Combining

--- Data-Analysis/DataFrame.py
+++ Data-Analysis/DataFrame.py
@@ -44,3 +44,46 @@ print(df2)
 # 1  1.2  3
 
 #drop: drop rows or cloumns (using labels=''&axis='' or index='', cloumns='')
+
+#contact
+df1 = pd.DataFrame({'c1':[1,2], 'c2':[3,4]},
+                   index=['r1','r2'])
+df2 = pd.DataFrame({'c1':[5,6], 'c2':[7,8]},
+                   index=['r1','r2'])
+df3 = pd.DataFrame({'c1':[5,6], 'c2':[7,8]})
+
+concat = pd.concat([df1, df2], axis=1)
+print(concat)
+#     c1  c2  c1  c2
+# r1   1   3   5   7
+# r2   2   4   6   8
+
+concat = pd.concat([df1, df3])
+print(concat)
+#     c1  c2
+# r1   1   3
+# r2   2   4
+# 0    5   7
+# 1    6   8
+
+concat = pd.concat([df1, df3], axis=1) #match labels
+print(concat)
+#      c1   c2   c1   c2
+# r1  1.0  3.0  NaN  NaN
+# r2  2.0  4.0  NaN  NaN
+# 0   NaN  NaN  5.0  7.0
+# 1   NaN  NaN  6.0  8.0
+
+
+#merge
+#the rows with same common cloumns will be merged if no keyword arguments given
+df1 = pd.DataFrame({'name' : ['zxj', 'hyc', 'wrj'],
+                    'sex' : ['M', 'M', 'M'],
+                    'money' : [100, 100000, 100]})
+df2 = pd.DataFrame({'name' : ['zxj', 'hyc', 'wrj'],
+                    'sex' : ['M', 'M', 'F'],
+                    'year' : [1000, 1000, 100]})
+print(pd.merge(df1, df2))
+#     money name sex  year
+# 0     100  zxj   M  1000
+# 1  100000  hyc   M  1000
